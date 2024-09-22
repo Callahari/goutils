277e0fd8a1a1c9198ac6261b8471015b6d34e010
Florian Krick <florian-krick@web.de>
2024-09-22 20:22:20 +0200

    Add Some source Documentation an refactor the RandomDateBetween func

--- random.go
+++ random.go
@@ -5,23 +5,30 @@ import (
 	"time"
 )
 
+// RandomIntBetween returns a random integer between the specified min and max values (inclusive).
 func RandomIntBetween(min, max int) int {
 	return rand.Intn(max-min+1) + min
 }
 
+// RandomFloat32Between returns a random float32 value in the range [min, max).
 func RandomFloat32Between(min, max float32) float32 {
 	return min + rand.Float32()*(max-min)
 }
 
+// RandomFloat64Between returns a random float64 number between the specified min and max values.
 func RandomFloat64Between(min, max float64) float64 {
 	return min + rand.Float64()*(max-min)
 }
 
+// RandomDateBetween generates a random date between the given startDate and endDate.
 func RandomDateBetween(startDate, endDate time.Time) time.Time {
-	//min := time.Date(2023, 12, 1, 0, 0, 0, 0, time.UTC).Unix()
-	//max := time.Date(2023, 12, 31, 23, 0, 0, 0, time.UTC).Unix()
-	delta := endDate.Unix() - startDate.Unix()
-
-	sec := rand.Int63n(delta) + startDate.Unix()
-	return time.Unix(sec, 0)
+	if startDate.After(endDate) {
+		startDate, endDate = endDate, startDate
+	}
+	duration := endDate.Sub(startDate)
+	if duration == 0 {
+		return startDate
+	}
+	randomDuration := time.Duration(rand.Int63n(int64(duration)))
+	return startDate.Add(randomDuration)
 }
